7864ba81eec3726ec3682a5d0580cab89d30ef90
Octipus <bmx_tavi@yahoo.com>
2019-10-21 15:20:50 +0100

    contact form handler

--- index.js
+++ index.js
@@ -1,10 +1,59 @@
 const path = require("path");
 const express = require("express");
+const nodeMailer = require("nodemailer");
+const bodyParser = require('body-parser');
 
 const app = express();
 const port = process.env.PORT || "8000";
 
 app.use(express.static(path.join(__dirname, "public")));
+
+//contact form handler
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.post('/send-email', function (req, res) {
+
+  var body = req.body;
+  var name = body.name;
+  var email = body.email;
+  var social = body.social;
+  var message = body.message;
+
+  var composedMessage = {
+      text: 'Hey Dan!\n\n' +
+        `${name} has contacted you through your website. Here is their contact information and message: \n\n` +
+        `Name: ${name} \n` +
+        `Email Address: ${email} \n` +
+        `Social: ${social} \n` +
+        `Message: ${message} \n\n`,
+      subject: 'Website Contact Form'
+    };
+
+  let transporter = nodeMailer.createTransport({
+      host: 'smtp.gmail.com',
+      port: 465,
+      secure: true,
+      auth: {
+          // should be replaced with real sender's account
+          user: '',
+          pass: ''
+      }
+  });
+
+  transporter.sendMail({
+      from: `${name}`,
+      to: 'octavian@justcatalyst.com',
+      subject: composedMessage.subject,
+      text: composedMessage.text
+    }, (error, info) => {
+      if (error) {
+        return console.log(error);
+      } else {
+        res.redirect("contact");
+      }
+    });
+
+});
 
 
 // Views
