801e68697eee0efff7a83ad9f526815e01995dbd
Octipus <bmx_tavi@yahoo.com>
2019-10-22 12:25:48 +0100

    title mouse-effect

--- public/js/glitch.js
+++ public/js/glitch.js
@@ -1,6 +1,23 @@
+//Glitch effect
 {
 	imagesLoaded('.glitch__img', { background: true }, () => {
 		document.body.classList.remove('loading');
 		document.body.classList.add('imgloaded');
 	});
 }
+
+//Title animation on mousemove
+$(document).mousemove(function(event){
+   var xPos = (event.clientX/$(window).width())-0.5,
+       yPos = (event.clientY/$(window).height())-0.5,
+       box = $('.box'),
+       coord = $('.coordinates');
+
+  TweenLite.to(box, 0.6, {
+    rotationY: 15 * xPos,
+    rotationX: 15 * yPos,
+    ease: Power1.easeOut,
+    transformPerspective: 900,
+    transformOrigin: 'center'
+  });
+});
