e629c1d950a98a2eef99763c11d0a2e688d16018
Octavian Contis <bmx_tavi@yahoo.com>
2020-08-17 14:08:58 +0100

    August2020 updates

--- index.js
+++ index.js
@@ -118,6 +118,46 @@ app.get("/sitemap.xml", (req, res) => {
   res.render("sitemap", { title: "siemap" });
 });
 
+// Load video file
+app.get('/video', function(req, res) {
+  const path = 'public/media/fitness-bg.webm'
+  const stat = fs.statSync(path)
+  const fileSize = stat.size
+  const range = req.headers.range
+
+  if (range) {
+    const parts = range.replace(/bytes=/, "").split("-")
+    const start = parseInt(parts[0], 10)
+    const end = parts[1]
+      ? parseInt(parts[1], 10)
+      : fileSize-1
+
+    if(start >= fileSize) {
+      res.status(416).send('Requested range not satisfiable\n'+start+' >= '+fileSize);
+      return
+    }
+
+    const chunksize = (end-start)+1
+    const file = fs.createReadStream(path, {start, end})
+    const head = {
+      'Content-Range': `bytes ${start}-${end}/${fileSize}`,
+      'Accept-Ranges': 'bytes',
+      'Content-Length': chunksize,
+      'Content-Type': 'video/webm',
+    }
+
+    res.writeHead(206, head)
+    file.pipe(res)
+  } else {
+    const head = {
+      'Content-Length': fileSize,
+      'Content-Type': 'video/webm',
+    }
+    res.writeHead(200, head)
+    fs.createReadStream(path).pipe(res)
+  }
+})
+
 
 
 // Listen both http & https ports
